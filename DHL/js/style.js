ef6464feb2e1effa8f9a04d2c12d6bbf582b2165
Blendero0 <zugmarketprof@gmail.com>
2021-06-19 14:18:02 +0300

    commit dhl

---
+++ DHL/js/style.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.search__button').click(function(event){
+        $('.search__button,.search__input').toggleClass('active__input');
+    })
+})
+
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header__menu').toggleClass('active__menu');
+    })
+})
+
